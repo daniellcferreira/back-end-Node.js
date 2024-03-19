02cfef95d7c445caf1a6c955e4c720f1c4113c23
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2024-03-19 10:55:18 -0300

    update lesson

---
+++ aula-typescript/tipagem-02.ts
@@ -0,0 +1,14 @@
+interface User {
+  nome: string;
+  email: string;
+}
+
+const usuarioLogado: User = {
+  nome: "Fulado",
+  email: "fulado@teste.com",
+};
+
+const usuarioLogado2: User = {
+  nome: "Ciclano",
+  email: "ciclano@teste.com",
+};
---
+++ aula-typescript/tipagem.ts
@@ -0,0 +1,5 @@
+let numero1: number;
+
+let numero = 10;
+
+console.log(numero);
